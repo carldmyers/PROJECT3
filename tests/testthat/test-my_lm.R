6eed00b088a83db6d2537510fd5ebca976fcb9ba
Carl Myers <cdmyers6@gmail.com>
2020-03-11 16:17:47 -0700

    test updates

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,7 +1,6 @@
 #within test-my_lm.R
-skinner <- my_lm(realLm, data = mtcars)
 test_that("my_lm outputs a table", {
-  expect_is(my_lm(pop ~ gdpPercap, my_data = my_gapminder), "table")
+  expect_is(my_lm(realLm, data = mtcars), "table")
 })
 test_that("non-table input throws error", {
   expect_error(my_lm("a string"))
--- tests/testthat/test-my_rf_cv.R
+++ tests/testthat/test-my_rf_cv.R
@@ -1,7 +1,7 @@
 #within test-my_rf_cv.R
 test_that("my_rf_cv outputs a numeric", {
-  expect_is(my_rf_cv(5), "numeric")
+  expect_is(my_rf_cv(k = 5), "numeric")
 })
 test_that("non-numeric input throws error", {
-  expect_error(my_rf_cv("a string"))
+  expect_error(my_rf_cv("character"))
 })
