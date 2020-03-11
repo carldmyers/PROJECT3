0e37dccb62ae72e11f1ae48bb4e57b7a1730263e
Carl Myers <cdmyers6@gmail.com>
2020-03-11 15:55:55 -0700

    travis capitalization

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -4,7 +4,7 @@ test_that("estimates are correct", {
   expect_equal(as.vector(skinner[, 1]), c(37.22727012, -0.03177295, -3.87783074))
 })
 test_that("my_lm outputs a table", {
-  expect_is(my_lm(realLm, data = mtcars), "table")
+  expect_is(my_lm(pop ~ gdpPercap, my_data = my_gapminder), "table")
 })
 test_that("non-table input throws error", {
   expect_error(my_lm("a string"))
